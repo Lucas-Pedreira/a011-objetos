f53b28fbcca9dd1b75141b035d343acb34be2db0
Lucas-Pedreira <lucasrpedreira@gmail.com>
2022-09-05 12:35:10 -0300

    ex1feito

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+const sacolao = []
+
+const fruta1= {nome: "banana", preço: 7, disponibilidade:true}
+const fruta2= {nome: "maça", preço: 5, disponibilidade:true,}
+const fruta3= {nome: "caju", preço: 10, disponibilidade:false}
+
+sacolao.push(fruta1, fruta2, fruta3)
+
+console.log(sacolao)
